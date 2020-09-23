2b3db36eca8177890d93f971fba179339480ee6c
stazeng <36391796+stazeng@users.noreply.github.com>
2020-09-23 22:13:43 +0800

    Update after-feeds-inst.sh

--- after-feeds-inst.sh
+++ after-feeds-inst.sh
@@ -2,13 +2,6 @@
 #
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
 
 # Clone install program
 git clone https://github.com/tuanqing/install-program package/install-program
