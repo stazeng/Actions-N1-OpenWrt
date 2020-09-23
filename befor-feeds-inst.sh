77e92a283eb54f66a0a34844e38ccdd6676f122a
stazeng <36391796+stazeng@users.noreply.github.com>
2020-09-23 23:06:13 +0800

    Update befor-feeds-inst.sh

--- befor-feeds-inst.sh
+++ befor-feeds-inst.sh
@@ -10,3 +10,17 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+#添加自定义插件链接
+#openclash出国软件
+#git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+#adguardhome广告过滤
+#svn co https://github.com/Lienol/openwrt/trunk/package/diy/adguardhome package/adguardhome  
+#svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome package/luci-app-adguardhome
+#atmaterial主题
+#svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/luci-theme-atmaterial package/luci-theme-atmaterial
+#smartdns
+#git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+#京东自动签到插件
+#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
+#git clone https://github.com/jerrykuku/node-request.git package/node-request
